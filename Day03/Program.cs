2d1d81ff712638a328800ee71b772eb453221647
Timur Kushukov <timqsh@gmail.com>
2024-12-16 22:44:32 +0100

    day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var text = File.ReadAllText("../../../input.txt");
+        var matches = Regex.Matches(text, @"(?:mul\((\d+),(\d+)\))|(?:do\(\))|(?:don't\(\))");
+        var result = 0;
+        var enabled = true;
+        foreach (Match match in matches)
+        {
+            if (match.Value.StartsWith("do()"))
+            {
+                enabled = true;
+                continue;
+            }
+            if (match.Value.StartsWith("don't"))
+            {
+                enabled = false;
+                continue;
+            }
+            if (!enabled)
+            {
+                continue;
+            }
+            var first = match.Groups[1].Value;
+            var second = match.Groups[2].Value;
+            result += Convert.ToInt32(first) * Convert.ToInt32(second);
+        }
+        Console.WriteLine(result);
+    }
+}
